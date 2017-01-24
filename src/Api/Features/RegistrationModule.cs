42bf7e0e16909b8db294c165348cf74fb67a2d78
Wes Shaddix <wes.shaddix@outlook.com>
2017-01-24 07:18:20 -0500

    back to .net 4.6.1

---
+++ src/Api/Features/RegistrationModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+
+namespace SiteWatchman.Api.Features
+{
+    public class RegistrationModule : NancyModule
+    {
+        public RegistrationModule() : base("/registrations")
+        {
+            Get["/"] = parameters => "List of registrations";
+            Get["/{id}"] = parameters => "Details of a single registration";
+            Post["/"] = parameters => "Create a new registration";
+            Patch["/{id}"] = parameters => "Patch a single registration";
+            Delete["/{id}"] = parameters => "Delete a single registration";
+        }
+    }
+}
